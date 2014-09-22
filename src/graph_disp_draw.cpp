872c142b07add1e9a64f487c497d0e8468d4fb46
mattvchandler <tardarsauce@gmail.com>
2014-09-22 11:05:35 -0800

    split graph_disp.cpp into 3 (seperate draw, input)

---
+++ src/graph_disp_draw.cpp
@@ -0,0 +1,248 @@
+// graph_disp_draw.cpp
+// Graphics display drawing code
+
+// Copyright 2014 Matthew Chandler
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include "gl_helpers.hpp"
+#include "graph_disp.hpp"
+
+void Cursor::draw() const
+{
+    // load the vertexes and texture
+    glBindVertexArray(_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+    glBindTexture(GL_TEXTURE_2D, _tex);
+
+    // draw the geometry
+    glDrawArrays(GL_TRIANGLES, 0, _num_indexes);
+}
+
+void Axes::draw() const
+{
+    // load the vertices and texture
+    glBindVertexArray(_vao);
+    glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+
+    // draw the geometry
+    glDrawArrays(GL_LINES, 0, _num_indexes);
+}
+
+void Graph_disp::graph_draw_setup(std::unordered_map<std::string, GLuint> & uniforms,
+    const Graph & graph, const glm::mat4 & view_model_perspective, const glm::mat4 & view_model,
+    const glm::mat3 & normal_transform, const glm::vec3 & dir_light_dir, const glm::vec3 & dir_half_vec)
+{
+    glUniformMatrix4fv(uniforms["view_model_perspective"], 1, GL_FALSE, &view_model_perspective[0][0]);
+    glUniformMatrix4fv(uniforms["view_model"], 1, GL_FALSE, &view_model[0][0]);
+    glUniformMatrix3fv(uniforms["normal_transform"], 1, GL_FALSE, &normal_transform[0][0]);
+
+    // material properties
+    if(!(graph.use_tex && graph.valid_tex))
+    {
+        glUniform4fv(uniforms["color"], 1, &graph.color[0]);
+    }
+    glUniform1f(uniforms["shininess"], graph.shininess);
+    glUniform3fv(uniforms["specular"], 1, &graph.specular[0]);
+    glUniform3fv(uniforms["dir_light_dir"], 1, &dir_light_dir[0]);
+    glUniform3fv(uniforms["dir_half_vec"], 1, &dir_half_vec[0]);
+
+    // light properties
+    glUniform3fv(uniforms["cam_light_color"], 1, &cam_light.color[0]);
+    glUniform1f(uniforms["cam_light_strength"], cam_light.strength);
+    glUniform1f(uniforms["const_atten"], cam_light.const_atten);
+    glUniform1f(uniforms["linear_atten"], cam_light.linear_atten);
+    glUniform1f(uniforms["quad_atten"], cam_light.quad_atten);
+    glUniform3fv(uniforms["dir_light_color"], 1, &dir_light.color[0]);
+    glUniform1f(uniforms["dir_light_strength"], dir_light.strength);
+}
+
+// main drawing code
+bool Graph_disp::draw(const Cairo::RefPtr<Cairo::Context> & unused)
+{
+    // set up global lights
+    glClearColor(bkg_color.r, bkg_color.g, bkg_color.b, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glUseProgram(_prog_tex);
+    glUniform3fv(_prog_tex_uniforms["ambient_color"], 1, &ambient_color[0]);
+
+    glUseProgram(_prog_color);
+    glUniform3fv(_prog_color_uniforms["ambient_color"], 1, &ambient_color[0]);
+
+    // set up viewmodel matrices
+    glm::mat4 view_model;
+    if(use_orbit_cam)
+    {
+        view_model = glm::rotate(glm::rotate(glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -_orbit_cam.r)),
+            -_orbit_cam.phi, glm::vec3(1.0f, 0.0f, 0.0f)), -_orbit_cam.theta, glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    else
+    {
+        view_model = _cam.view_mat();
+    }
+    view_model = glm::scale(view_model, glm::vec3(_scale));
+
+    glm::mat4 view_model_perspective = _perspective * view_model;
+    glm::mat3 normal_transform = glm::transpose(glm::inverse(glm::mat3(view_model)));
+
+    glm::vec3 light_forward(0.0f, 0.0f, 1.0f); // in eye space
+
+    // directional light
+    glm::vec3 dir_light_dir = normal_transform * glm::normalize(-dir_light.pos);
+    glm::vec3 dir_half_vec = glm::normalize(light_forward + dir_light_dir);
+
+    // draw axes
+    if(draw_axes_flag)
+    {
+        glUseProgram(_prog_line);
+
+        glUniformMatrix4fv(_prog_line_uniforms["perspective"], 1, GL_FALSE, &_perspective[0][0]);
+        glUniformMatrix4fv(_prog_line_uniforms["view_model"], 1, GL_FALSE, &view_model[0][0]);
+        glUniform4fv(_prog_line_uniforms["color"], 1, &_axes.color[0]);
+
+        _axes.draw();
+
+        check_error("draw axes");
+    }
+
+    // draw opaque graphs
+    for(auto &graph: _graphs)
+    {
+        // draw geometry
+        if(graph->draw_flag && !graph->transparent_flag)
+        {
+            if(graph->use_tex && graph->valid_tex)
+            {
+                // draw with texture
+                glUseProgram(_prog_tex);
+                graph_draw_setup(_prog_tex_uniforms, *graph,
+                    view_model_perspective, view_model, normal_transform,
+                    dir_light_dir, dir_half_vec);
+            }
+            else
+            {
+                // draw with one color
+                glUseProgram(_prog_color);
+                graph_draw_setup(_prog_color_uniforms, *graph,
+                    view_model_perspective, view_model, normal_transform,
+                    dir_light_dir, dir_half_vec);
+            }
+            check_error("draw geometry");
+
+            graph->draw();
+        }
+
+        // draw grid
+        if(graph->draw_grid_flag && !graph->transparent_flag)
+        {
+            // switch to line shader
+            glUseProgram(_prog_line);
+            glUniformMatrix4fv(_prog_line_uniforms["perspective"], 1, GL_FALSE, &_perspective[0][0]);
+            glUniformMatrix4fv(_prog_line_uniforms["view_model"], 1, GL_FALSE, &view_model[0][0]);
+            glUniform4fv(_prog_line_uniforms["color"], 1, &graph->grid_color[0]);
+
+            graph->draw_grid();
+            check_error("draw grid");
+        }
+
+        // draw normal vectors
+        if(graph->draw_normals_flag)
+        {
+            // switch to line shader
+            glUseProgram(_prog_line);
+            glUniformMatrix4fv(_prog_line_uniforms["perspective"], 1, GL_FALSE, &_perspective[0][0]);
+            glUniformMatrix4fv(_prog_line_uniforms["view_model"], 1, GL_FALSE, &view_model[0][0]);
+            glUniform4fv(_prog_line_uniforms["color"], 1, &graph->normal_color[0]);
+
+            graph->draw_normals();
+            check_error("draw normals");
+        }
+    }
+
+    // draw cursor
+    if(draw_cursor_flag && _active_graph && _active_graph->cursor_defined())
+    {
+        glUseProgram(_prog_tex);
+
+        glm::mat4 cursor_view_model = glm::scale(glm::translate(view_model, _active_graph->cursor_pos()), glm::vec3(0.25f / _scale));
+        glm::mat4 cursor_view_model_perspective = _perspective * cursor_view_model;
+        glm::mat3 cursor_normal_transform = glm::transpose(glm::inverse(glm::mat3(cursor_view_model)));
+
+        glUniformMatrix4fv(_prog_tex_uniforms["view_model_perspective"], 1, GL_FALSE, &cursor_view_model_perspective[0][0]);
+        glUniformMatrix4fv(_prog_tex_uniforms["view_model"], 1, GL_FALSE, &cursor_view_model[0][0]);
+        glUniformMatrix3fv(_prog_tex_uniforms["normal_transform"], 1, GL_FALSE, &cursor_normal_transform[0][0]);
+
+        // material properties
+        glUniform1f(_prog_tex_uniforms["shininess"], _cursor.shininess);
+        glUniform3fv(_prog_tex_uniforms["specular"], 1, &_cursor.specular[0]);
+        glUniform3fv(_prog_tex_uniforms["dir_light_dir"], 1, &dir_light_dir[0]);
+        glUniform3fv(_prog_tex_uniforms["dir_half_vec"], 1, &dir_half_vec[0]);
+
+        _cursor.draw();
+        check_error("draw cursor");
+    }
+
+    // 2nd pass to draw transparent graphs
+    for(auto &graph: _graphs)
+    {
+        glDepthMask(GL_FALSE);
+        glBlendFunc(GL_SRC_COLOR, GL_ONE);
+        // draw geometry
+        if(graph->draw_flag && graph->transparent_flag)
+        {
+            if(graph->use_tex && graph->valid_tex)
+            {
+                // draw with texture
+                glUseProgram(_prog_tex);
+                graph_draw_setup(_prog_tex_uniforms, *graph,
+                    view_model_perspective, view_model, normal_transform,
+                    dir_light_dir, dir_half_vec);
+            }
+            else
+            {
+                // draw with one color
+                glUseProgram(_prog_color);
+                graph_draw_setup(_prog_color_uniforms, *graph,
+                    view_model_perspective, view_model, normal_transform,
+                    dir_light_dir, dir_half_vec);
+            }
+            check_error("draw transparent geometry");
+
+            graph->draw();
+        }
+
+        // draw grid
+        if(graph->draw_grid_flag && graph->transparent_flag)
+        {
+            // switch to line shader
+            glUseProgram(_prog_line);
+            glUniformMatrix4fv(_prog_line_uniforms["perspective"], 1, GL_FALSE, &_perspective[0][0]);
+            glUniformMatrix4fv(_prog_line_uniforms["view_model"], 1, GL_FALSE, &view_model[0][0]);
+            glUniform4fv(_prog_line_uniforms["color"], 1, &graph->grid_color[0]);
+
+            graph->draw_grid();
+            check_error("draw grid");
+        }
+        glDepthMask(GL_TRUE);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    }
+
+    display(); // swap display buffers
+    return true;
+}
---
+++ src/graph_disp_input.cpp
@@ -0,0 +1,271 @@
+// graph_disp_input.cpp
+// Graphics display mouse & keyboard input
+
+// Copyright 2014 Matthew Chandler
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to
+// use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+// FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+// COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+// IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include "graph_disp.hpp"
+
+// main input processing
+bool Graph_disp::input()
+{
+    // state vars
+    static std::unordered_map<sf::Keyboard::Key, bool, std::hash<int>> key_lock;
+    static sf::Vector2i old_mouse_pos = sf::Mouse::getPosition(glWindow);
+    static sf::Clock cursor_delay;
+    static sf::Clock zoom_delay;
+
+    // the neat thing about having this in a timeout func is that we
+    // don't need to calculate dt for movement controls.
+    // it is always (almost) exactly 10ms
+
+    // only process when the window is active and display is focused
+    if(dynamic_cast<Gtk::Window *>(get_toplevel())->is_active())
+    {
+        sf::Vector2i new_mouse_pos = sf::Mouse::getPosition(glWindow);
+
+        if(!has_focus() && new_mouse_pos.x >= 0 && new_mouse_pos.y >= 0 &&
+            new_mouse_pos.x < get_allocated_width() && new_mouse_pos.y < get_allocated_height())
+        {
+            grab_focus();
+        }
+
+        if(has_focus())
+        {
+            // Camera controls
+
+            float mov_scale = 0.1f;
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::LShift))
+            {
+                mov_scale *= 2.0f;
+            }
+
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::LAlt))
+            {
+                mov_scale *= 0.1f;
+            }
+
+            // orbiting rotational cam
+            if(use_orbit_cam)
+            {
+                // reset
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::R) && !key_lock[sf::Keyboard::R])
+                {
+                    key_lock[sf::Keyboard::R] = true;
+                    reset_cam();
+                }
+                else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+                    key_lock[sf::Keyboard::R] = false;
+
+                // tilt up
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                {
+                    _orbit_cam.phi += (float)M_PI / 90.0f * mov_scale;
+                    invalidate();
+                }
+
+                // tilt down
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    _orbit_cam.phi -= (float)M_PI / 90.0f * mov_scale;
+                    invalidate();
+                }
+
+                // rotate clockwise
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                {
+                    _orbit_cam.theta += (float)M_PI / 90.0f * mov_scale;
+                    invalidate();
+                }
+
+                // rotate counter-clockwise
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    _orbit_cam.theta -= (float)M_PI / 90.0f * mov_scale;
+                    invalidate();
+                }
+
+                // move camera in
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+                {
+                    _orbit_cam.r -= mov_scale;
+                    invalidate();
+                }
+
+                // move camera out
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+                {
+                    _orbit_cam.r += mov_scale;
+                    invalidate();
+                }
+
+                // rotate w/ mouse click & drag
+                if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                {
+                    int d_x = new_mouse_pos.x - old_mouse_pos.x;
+                    int d_y = new_mouse_pos.y - old_mouse_pos.y;
+
+                    _orbit_cam.theta -= 0.005f * d_x;
+                    _orbit_cam.phi -= 0.005f * d_y;
+
+                    invalidate();
+                }
+
+                // wrap theta
+                if(_orbit_cam.theta > (float)M_PI * 2.0f)
+                    _orbit_cam.theta -= (float)M_PI * 2.0f;
+                if(_orbit_cam.theta < 0.0f)
+                    _orbit_cam.theta += (float)M_PI * 2.0f;
+
+                // clamp phi
+                if(_orbit_cam.phi > (float)M_PI)
+                    _orbit_cam.phi = (float)M_PI;
+                if(_orbit_cam.phi < 0.0f)
+                    _orbit_cam.phi = 0.0f;
+            }
+            else // free camera
+            {
+                // reset
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::R) && !key_lock[sf::Keyboard::R])
+                {
+                    key_lock[sf::Keyboard::R] = true;
+                    reset_cam();
+                }
+                else if(!sf::Keyboard::isKeyPressed(sf::Keyboard::R))
+                    key_lock[sf::Keyboard::R] = false;
+
+                // move forward
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                {
+                    _cam.translate(mov_scale * _cam.forward());
+                    invalidate();
+                }
+
+                // move backwards
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    _cam.translate(-mov_scale * _cam.forward());
+                    invalidate();
+                }
+
+                // move left
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                {
+                    _cam.translate(-mov_scale * _cam.right());
+                    invalidate();
+                }
+
+                // move right
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    _cam.translate(mov_scale * _cam.right());
+                    invalidate();
+                }
+
+                // move up
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+                {
+                    _cam.translate(mov_scale * glm::vec3(0.0f, 0.0f, 1.0f));
+                    invalidate();
+                }
+
+                // move down
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+                {
+                    _cam.translate(-mov_scale * glm::vec3(0.0f, 0.0f, 1.0f));
+                    invalidate();
+                }
+
+                // rotate view with mouse click & drag
+                if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                {
+                    int d_x = new_mouse_pos.x - old_mouse_pos.x;
+                    int d_y = new_mouse_pos.y - old_mouse_pos.y;
+
+                    _cam.rotate(0.001f * d_y, _cam.right());
+                    _cam.rotate(0.001f * d_x, glm::vec3(0.0f, 0.0f, 1.0f));
+
+                    invalidate();
+                }
+            }
+
+            const int zoom_timeout = 200;
+
+            // zoom in
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::Z) && zoom_delay.getElapsedTime().asMilliseconds() >= zoom_timeout)
+            {
+                _scale *= 2.0f;
+                zoom_delay.restart();
+                invalidate();
+            }
+
+            // zoom out
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::X) && zoom_delay.getElapsedTime().asMilliseconds() >= zoom_timeout)
+            {
+                _scale *= 0.5f;
+                zoom_delay.restart();
+                invalidate();
+            }
+
+            // move cursor with arrow keys
+            if(draw_cursor_flag && _active_graph)
+            {
+                const int cursor_timeout = 200;
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && cursor_delay.getElapsedTime().asMilliseconds() >= cursor_timeout)
+                {
+                    _active_graph->move_cursor(Graph::UP);
+                    cursor_delay.restart();
+                    invalidate();
+                }
+
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && cursor_delay.getElapsedTime().asMilliseconds() >= cursor_timeout)
+                {
+                    _active_graph->move_cursor(Graph::DOWN);
+                    cursor_delay.restart();
+                    invalidate();
+                }
+
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && cursor_delay.getElapsedTime().asMilliseconds() >= cursor_timeout)
+                {
+                    _active_graph->move_cursor(Graph::LEFT);
+                    cursor_delay.restart();
+                    invalidate();
+                }
+
+                if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && cursor_delay.getElapsedTime().asMilliseconds() >= cursor_timeout)
+                {
+                    _active_graph->move_cursor(Graph::RIGHT);
+                    cursor_delay.restart();
+                    invalidate();
+                }
+            }
+        }
+        old_mouse_pos = new_mouse_pos;
+    }
+    return true;
+}
+
+// GTK key press handler
+bool Graph_disp::key_press(GdkEventKey * e)
+{
+    // returning true means we don't propagate key presses up - keep them in this widget
+    // (and disregard them entirely because we're using SFML for handling the keyboard, not GTK)
+    return true;
+}
